49cf14e8704a99beefc3e2114ca202d607bf29a8
Chirayu Desai <chirayudesai1@gmail.com>
2015-02-10 17:45:19 +0100

    flamingo: Allow inline kernel building
    Define the device's kernel defconfig
    
    for inline kernel building we have to define
    kernel config for each device

--- aosp_d2203.mk
+++ aosp_d2203.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_PREBUILT_KERNEL := kernel
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_PREBUILT_KERNEL):kernel
+TARGET_KERNEL_CONFIG := aosp_yukon_flamingo_defconfig
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # Include others
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
